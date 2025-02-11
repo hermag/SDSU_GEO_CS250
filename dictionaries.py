a77e1e12fe3c6528b007ffca7b75988e4465a0c3
Erekle Magradze <erekle@magradze.de>
2025-02-11 10:47:35 +0400

    adding content

---
+++ dictionaries.py
@@ -0,0 +1,12 @@
+#To store the key value pairs, python uses dictionary
+
+dict = {} #declaration of empty dictionary
+
+#To fill the dictionary with elements
+dict["key1"]="value1"
+
+#dict={"key1":"value1"}
+
+#dictionary, as well as lists can be embeded
+dict={"key1":[1,2,3,4,5,6,7,8,9,10]}
+dict={"key1":[1,2,3,4,5,6,7,8,9,10],"key2":{"key3":"value3"}}
---
+++ functions.py
@@ -0,0 +1,5 @@
+#declaration of function
+
+def func1(arg1, arg2):
+    print(arg1, arg2)
+    return arg1+arg2
