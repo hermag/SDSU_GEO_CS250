08f7f382299c6ffc07435c0869ffce835a5a1044
Erekle Magradze <emagradze@argo.local>
2025-05-05 21:40:51 +0400

    adding icc internship students

---
+++ Flask_2/app_html_extensions/example_4/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "my super secret key"
+
+#Create a form class
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+@app.route('/user/<name>',methods=['GET', 'POST'])
+def index(name):
+    name = None 
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        print("=====>  name is", name)
+        form.name.data = ''
+    return render_template('name.html', name=name, form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True) 
